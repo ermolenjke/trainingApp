c9ac1f39d05172b2448456ed024497c9e2aea50c
ermolenjke <ermolenko.daniil.al@gmail.com>
2022-04-27 13:37:20 +0300

    ...

--- traningApp/View/MainVC/Cells/WorkoutTableViewCell.swift
+++ traningApp/View/MainVC/Cells/WorkoutTableViewCell.swift
@@ -8,6 +8,56 @@
 import UIKit
 
 class WorkoutTableViewCell: UITableViewCell {
+    
+    private let exerciseImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = #colorLiteral(red: 0.9411764706, green: 0.9294117647, blue: 0.8862745098, alpha: 1)
+        imageView.layer.cornerRadius = 20
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named:"pullUpsImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.layer.cornerRadius = 10
+        button.setTitle("START", for: .normal)
+        button.titleLabel?.font = .robotoBold16()
+        button.tintColor = .specialDarkGreen
+        button.backgroundColor = .specialYellow
+        button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        button.addShadowOnView()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let setsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sets: 2"
+        label.font = .robotoMedium14()
+        label.textColor = .specialGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let repsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Reps: 10"
+        label.font = .robotoMedium14()
+        label.textColor = .specialGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let exerciseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pull Ups"
+        label.font = .robotoMedium24()
+        label.textColor = .specialBlack
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     private let backgroundCell: UIView = {
        let view = UIView()
@@ -33,6 +83,16 @@ class WorkoutTableViewCell: UITableViewCell {
         selectionStyle = .none
         
         addSubview(backgroundCell)
+        addSubview(exerciseImageView)
+        addSubview(startButton)
+        addSubview(exerciseLabel)
+        addSubview(repsLabel)
+        addSubview(setsLabel)
+        
+    }
+    
+    @objc private func startButtonTapped() {
+        print(#function)
     }
     
     private func setConstraints() {
@@ -42,5 +102,43 @@ class WorkoutTableViewCell: UITableViewCell {
             backgroundCell.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
             backgroundCell.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0)
         ])
+        
+        NSLayoutConstraint.activate([
+            exerciseImageView.topAnchor.constraint(equalTo: backgroundCell.topAnchor, constant: 10),
+            exerciseImageView.leadingAnchor.constraint(equalTo: backgroundCell.leadingAnchor,constant: 8),
+            exerciseImageView.heightAnchor.constraint(equalToConstant: 75),
+            exerciseImageView.widthAnchor.constraint(equalToConstant: 75)
+        ])
+        
+        NSLayoutConstraint.activate([
+            startButton.bottomAnchor.constraint(equalTo: backgroundCell.bottomAnchor, constant: -10),
+            startButton.trailingAnchor.constraint(equalTo: backgroundCell.trailingAnchor, constant: -10),
+            startButton.heightAnchor.constraint(equalToConstant: 29),
+            startButton.widthAnchor.constraint(equalToConstant: 254)
+        ])
+        
+        NSLayoutConstraint.activate([
+            exerciseLabel.topAnchor.constraint(equalTo: backgroundCell.topAnchor, constant: 10),
+            exerciseLabel.leadingAnchor.constraint(equalTo: exerciseImageView.trailingAnchor, constant: 10),
+            exerciseLabel.trailingAnchor.constraint(equalTo: backgroundCell.trailingAnchor, constant: -100),
+            exerciseLabel.heightAnchor.constraint(equalToConstant: 26)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            repsLabel.bottomAnchor.constraint(equalTo: startButton.topAnchor, constant: -2),
+            repsLabel.leadingAnchor.constraint(equalTo: exerciseImageView.trailingAnchor, constant: 10),
+            repsLabel.heightAnchor.constraint(equalToConstant: 19),
+            repsLabel.heightAnchor.constraint(equalToConstant: 65)
+        ])
+        
+        NSLayoutConstraint.activate([
+            setsLabel.bottomAnchor.constraint(equalTo: startButton.topAnchor, constant: -2),
+            setsLabel.leadingAnchor.constraint(equalTo: repsLabel.trailingAnchor, constant: 10),
+            setsLabel.heightAnchor.constraint(equalToConstant: 19),
+            setsLabel.heightAnchor.constraint(equalToConstant: 65)
+            
+        ])
+        
     }
 }
